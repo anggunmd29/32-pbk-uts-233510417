5de028757b5eaf604a030f68dc1ae4cb1a369639
anggun <anggunmustikadewi@student.uir.ac.id>
2025-05-06 18:25:24 +0700

    Membuat Setup Dasar dan Fungsi Tambah Item

---
+++ src/components/ShoppingList.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue';
+
+const shoppingItems = ref([]);
+const newItem = ref('');
+
+const addItem = () => {
+  if (newItem.value.trim()) {
+    shoppingItems.value.push({ text: newItem.value, purchased: false });
+    newItem.value = '';
+  }
+};
+</script>
+
+<template>
+<div class="shopping-container">
+    <input
+      v-model="newItem"
+      placeholder="Masukkan item belanja"
+      class="input-field"
+    />
+    <button @click="addItem" class="btn-add">Tambah</button>
+
+    <ul class="item-list">
+      <li
+        v-for="(item, index) in shoppingItems"
+        :key="index"
+        class="item"
+      >
+        <span>{{ item.text }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
