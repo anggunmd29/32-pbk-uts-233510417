bc81afbd4c80221743481869f094b88506eec53b
anggun <anggunmustikadewi@student.uir.ac.id>
2025-05-06 17:49:12 +0700

    mulai projek

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import ShoppingList from './components/ShoppingList.vue';
+</script>
+
+<template>
+  <div id="app">
+    <h1 class="title">Daftar Belanja🐻</h1>
+    <ShoppingList/>
+  </div>
+</template>
+
+<style src="./style.css"></style>
+
